7fc3c558d3a54fdabfe62feb4faacd242edc5276
Olivier <fernandbakoulongo@gmail.com>
2026-01-23 09:57:44 +0100

    maj

--- edblose/settings.py
+++ edblose/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'edblose.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # templates globaux
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
