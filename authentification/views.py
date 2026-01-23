b76d5850c7867b10da0309101e611ff0a03bfc8a
gaella2012 <gaella2012@gmail.com>
2026-01-23 15:35:18 +0100

    mise à jour 1

--- authentification/views.py
+++ authentification/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+import datetime
+def index(request):
+	context={
+	"message":"Hello word !!",
+	"numberNews":9,
+	"usersList":["Papa","Olivier","BAKOU","Fernand"],
+	"publication":datetime.datetime.now(),
+	"username":"BAKOU LONGO Olivier Fernand"
+	}
+	template=loader.get_template("authentification/login.html")
+	return HttpResponse(template.render(context,request))
